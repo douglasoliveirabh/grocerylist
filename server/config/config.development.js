edfa6014b1d8974c34aa2543f2d28cad7f41cc1c
Douglas Everton de Oliveira <douglas.oliveira@squadra.com.br>
2017-01-04 14:25:21 -0200

    Prepare application for test implementation

---
+++ server/config/config.development.js
@@ -0,0 +1,14 @@
+export default {
+    database: {
+        options : {
+            user: 'mylist',
+            pass: 'mylist'
+        },
+        connectionstring : 'mongodb://ds139198.mlab.com:39198/mylist'
+    },
+    logging: {
+        format: 'dev',
+        options: {}
+    },
+    port: 3000
+};
